3fa3f1e2457377cc9ed89feaca5e404dac760cc3
Bozhin Katsarski <ozhi@users.noreply.github.com>
2018-08-20 17:48:09 +0300

    Use new plugin api (#1)

--- myplugin/myplugin.go
+++ myplugin/myplugin.go
@@ -10,16 +10,12 @@ type MyPlugin struct{}
 
 func (p *MyPlugin) Name() string { return "MyPlugin" }
 
-func (p *MyPlugin) FetchCatalog(next web.Handler) web.Handler {
-	handle := func(req *web.Request) (*web.Response, error) {
-		res, err := next.Handle(req)
-		if err != nil {
-			return nil, err
-		}
-		serviceName := gjson.GetBytes(res.Body, "services.0.name").String()
-		res.Body, err = sjson.SetBytes(res.Body, "services.0.name", serviceName+"-suffix")
-		return res, err
+func (p *MyPlugin) FetchCatalog(req *web.Request, next web.Handler) (*web.Response, error) {
+	res, err := next.Handle(req)
+	if err != nil {
+		return nil, err
 	}
-
-	return web.HandlerFunc(handle)
+	serviceName := gjson.GetBytes(res.Body, "services.0.name").String()
+	res.Body, err = sjson.SetBytes(res.Body, "services.0.name", serviceName+"-suffix")
+	return res, err
 }
